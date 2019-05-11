78a1435e3fe4a2fc1e0c0ff20f1ad9664f75169d
gitlab123 <flwwd@outlook.com>
2019-05-11 22:08:33 +0800

    list files

--- fileserver/main.go
+++ fileserver/main.go
@@ -3,22 +3,22 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
-	Initialize()
 	addr:= flag.String("addr", config.Host, "http service address")
-	BindHandlers()
 	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
 
-func Initialize()  {
+func init()  {
 	config=ReadConfig()
+	BindHandlers()
 }
 
 func BindHandlers(){
@@ -27,4 +27,12 @@ func BindHandlers(){
 
 func FileList(w http.ResponseWriter, req *http.Request){
 	fmt.Fprintln(w,config.FileLocation)
+	files, err := ioutil.ReadDir(config.FileLocation)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		fmt.Fprintln(w,f.Name())
+	}
 }
