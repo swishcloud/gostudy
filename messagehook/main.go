394423b2644ac615037418729eff3d60c62e8686
gitlab123 <flwwd@outlook.com>
2019-06-12 15:31:11 +0800

    windows message hook

---
+++ messagehook/main.go
@@ -0,0 +1,97 @@
+package messagehook
+
+import (
+	"fmt"
+	"syscall"
+	"unsafe"
+)
+
+const (
+	WH_KEYBOARD_LL = 13
+)
+
+func abort(funcname string, err error) {
+	panic(fmt.Sprintf("%s failed: %v", funcname, err))
+}
+
+var (
+	kernel32, _        = syscall.LoadLibrary("kernel32.dll")
+	getModuleHandle, _ = syscall.GetProcAddress(kernel32, "GetModuleHandleW")
+
+	user32, _            = syscall.LoadLibrary("user32.dll")
+	setWindowsHookExA, _ = syscall.GetProcAddress(user32, "SetWindowsHookExW")
+	getMessageW, _       = syscall.GetProcAddress(user32, "GetMessageW")
+)
+
+type (
+	DWORD uint32
+	WPARAM uintptr
+	LPARAM uintptr
+	LRESULT uintptr
+	HANDLE uintptr
+	HINSTANCE HANDLE
+	HHOOK HANDLE
+	HWND HANDLE
+	UINT uint32
+	ULONG_PTR uintptr
+)
+type CWPRETSTRUCT struct {
+	lResult LRESULT
+	lParam  LPARAM
+	wParam  WPARAM
+	message UINT
+	hwnd    HWND
+}
+
+type POINT struct {
+	X, Y int32
+}
+type Msg struct {
+	Hwnd    HWND
+	Message uint32
+	WParam  uintptr
+	LParam  uintptr
+	Time    uint32
+	Pt      POINT
+}
+type LPMSG *Msg
+type HOOKPROC func(int, WPARAM, LPARAM) LRESULT
+
+func SetWindowsHookExA(idHook int, lpfn HOOKPROC, hmod HINSTANCE, dwThreadId DWORD) (r HHOOK) {
+	var nargs uintptr = 4
+	l := syscall.NewCallback(lpfn)
+	if r1, _, err := syscall.Syscall6(uintptr(setWindowsHookExA), nargs, WH_KEYBOARD_LL, l, uintptr(hmod), 0, 0, 0); err != 0 {
+		abort("Call SetWindowsHookExA", err)
+	} else {
+		r = HHOOK(r1)
+	}
+	return
+}
+
+func GetMessageW(lpMsg LPMSG, hWnd HWND, wMsgFilterMin UINT, wMsgFilterMax UINT) (r int) {
+	var nargs uintptr = 4
+	if r1, _, err := syscall.Syscall6(uintptr(getMessageW), nargs, uintptr(unsafe.Pointer(lpMsg)), uintptr(hWnd), uintptr(wMsgFilterMax), uintptr(wMsgFilterMax), 0, 0); err != 0 {
+		abort("Call GetMessageW", err)
+	} else {
+		r = int(r1)
+	}
+	return
+}
+
+func GetModuleHandle() (handle uintptr) {
+	var nargs uintptr = 0
+	if ret, _, callErr := syscall.Syscall(uintptr(getModuleHandle), nargs, 0, 0, 0); callErr != 0 {
+		abort("Call GetModuleHandle", callErr)
+	} else {
+		handle = ret
+	}
+	return
+}
+func Start(h HOOKPROC) {
+	r := SetWindowsHookExA(WH_KEYBOARD_LL, h, HINSTANCE(GetModuleHandle()), 0)
+	if r == 0 {
+		panic("unexpected return value from SetWindowsHookExA:" + string(r))
+	}
+	var msg Msg
+	GetMessageW(LPMSG(&msg), 0, 0, 0)
+}
---
+++ shortcutkey/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"github.com/github-123456/gostudy/messagehook"
+	"unsafe"
+)
+
+type (
+	DWORD uint32
+	ULONG_PTR uint32
+)
+const (
+	WM_KEYUP   = 0x0101
+	WM_KEYDOWN = 0x0100
+	WM_SYSKEYUP=0x105
+	WM_SYSKEYDOWN=0x104
+)
+type KBDLLHOOKSTRUCT struct {
+	vkCode      DWORD
+	scanCode    DWORD
+	flags       DWORD
+	time        DWORD
+	dwExtraInfo ULONG_PTR
+}
+
+var keys = []int{}
+func main() {messagehook.Start(func(i int, wparam messagehook.WPARAM, lparam messagehook.LPARAM) messagehook.LRESULT {
+		data := (*KBDLLHOOKSTRUCT)(unsafe.Pointer(lparam))
+		if wparam == WM_KEYDOWN ||wparam==WM_SYSKEYDOWN{
+			keys = append(keys, int(data.vkCode))
+			if len(keys)>3{
+				keys=[]int{}
+			}
+			OnKeyDown()
+		} else if wparam == WM_KEYUP||wparam==WM_SYSKEYUP {
+			for i, v := range keys {
+				if v == int(data.vkCode) {
+					keys = append(keys[:i], keys[i+1:]...)
+					break
+				}
+			}
+		}
+		return messagehook.LRESULT(0)
+	})
+}
+
+
+func OnKeyDown() {
+	val:=0
+	for _,v:=range keys{
+		val=val|v
+	}
+	fmt.Println(keys,val)
+
+	//shit+alt+a shortcut
+	if val==229{
+		//todo something
+		fmt.Println("todo something")
+	}
+}
