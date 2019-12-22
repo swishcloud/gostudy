345f26ad171372b06c5959c1e9359b80d2504abb
swishcloud <flwwd@outlook.com>
2019-12-22 17:43:15 +0800

    add method NewLogger to package logger

--- logger/logger.go
+++ logger/logger.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
@@ -14,5 +15,9 @@ func NewFileLogger(filepath string, prefix string) *log.Logger {
 		os.Exit(1)
 	}
 	logger := log.New(f, prefix+" ", log.Ldate|log.Ltime|log.Lshortfile|log.LUTC)
+	return logger
+}
+func NewLogger(out io.Writer, prefix string) *log.Logger {
+	logger := log.New(out, prefix+" ", log.Ldate|log.Ltime|log.Lshortfile|log.LUTC)
 	return logger
 }
